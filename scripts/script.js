0e2b3771b7f7b7e3df17845c1767cbe9d61f2fbe
Astate PNG <92828727+AstatePNG@users.noreply.github.com>
2023-12-14 11:34:43 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,18 @@
+var menu = document.getElementById("menu");
+var burger = document.getElementById("burger");
+var list = document.getElementById("menu-list");
+var title = document.getElementById("labmenu");
+
 function showMenu() {
-    var menu = document.getElementById("menu");
-    var burger = document.getElementById("burger");
-    var list = document.getElementById("menu-list");
-    
     if (menu.classList.contains("open")) {
         menu.classList.remove("open");
         burger.classList.remove("open");
+        title.classList.remove("open");
         list.classList.remove("displayed");
     } else {
         menu.classList.add("open");
         burger.classList.add("open");
+        title.classList.add("open");
         list.classList.add("displayed");
     }
 }
